a37c843489af501382d77669e85345b855caa497
MSI\Lucas <lucasblon@orange.fr>
2019-01-09 15:41:17 +0100

    réalisation scene test

---
+++ Assets/Scripts/Prototyping/UI/Evenement_Interaction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evenement_Interaction : MonoBehaviour
+{
+
+    public GameObject infos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        infos.SetActive(true);
+        Debug.Log("test");
+    }
+
+    void OnMouseExit()
+    {
+        infos.SetActive(false);
+    }
+}
